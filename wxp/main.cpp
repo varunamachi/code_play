358ee948cb849a6d9a5a64f43baa5b37bf006d75
Varun Amachi <varunamachi@gmail.com>
2018-04-27 21:44:46 +0530

    Auth header parsing - WIP

--- wxp/main.cpp
+++ wxp/main.cpp
@@ -2,14 +2,61 @@
 #define CPPHTTPLIB_OPENSSL_SUPPORT
 
 #include <iostream>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <ctype.h>
+
 #include <crypto++/md5.h>
 
 
 #include "httplib.h"
 
-void streamDigest(std::vector< std::string > vals, std::stringstream &out) {
+inline std::string trim( const std::string &str )
+{
+   auto front = std::find_if_not(
+               str.begin(),
+               str.end(),
+               []( int c ) {
+       return std::isspace(c);
+   });
+   auto back = std::find_if_not(
+               str.rbegin(),
+               str.rend(),
+               [](int c) {
+       return std::isspace(c);
+   }).base();
+   return back <= front ? std::string() : std::string( front, back);
+}
+
+std::vector< std::string > && split( const std::string &val,
+                                     char delem ) {
+    std::vector< std::string > comps;
+    std::istringstream stream(val);
+    std::string temp;
+    while( std::getline( stream, temp, delem)) {
+        comps.push_back( trim( temp ));
+    }
+    return std::move( comps );
+}
+
+bool startsWith( const std::string &main, const std::string &check ) {
+    auto result = !check.empty();
+    for( std::string::size_type i = 0; i < check.size(); ++ i ) {
+        if( check[ i ] != main[ i ]) {
+            result = false;
+        }
+    }
+    return result;
+}
+
+
+
+std::string && getDigest(std::vector< std::string > &vals) {
     using CryptoPP::Weak::MD5;
     MD5 digester;
+    std::stringstream out;
     byte buffer[ MD5::DIGESTSIZE ];
     for( std::size_t i = 0; i < vals.size(); ++ i ) {
         const auto &val = vals[ i ];
@@ -22,6 +69,7 @@ void streamDigest(std::vector< std::string > vals, std::stringstream &out) {
     for( int i = 0; i < MD5::DIGESTSIZE; ++i ) {
         out << std::hex << static_cast< uint16_t >( buffer[i] );
     }
+    return std::move( out.str() );
 }
 
 void printHeaders( httplib::Response &res ) {
@@ -32,9 +80,36 @@ void printHeaders( httplib::Response &res ) {
     }
 }
 
+std::string && removeQuotes( const std::string &in ) {
+    if( in.size() > 2 ) {
+        auto first = std::begin( in );
+        auto last = std::end( in ) - 1;
+        if( *first == '"' ) {
+            first ++;
+        }
+        if( *last == '"' ) {
+            last ++;
+        }
+        return std::move( std::string{ first, last });
+    }
+    return std::move( std::string{} );
+}
+
+std::map< std::string, std::string > && authHeaderToMap( const std::string h ) {
+    std::map< std::string, std::string > map;
+    auto compsOne = split( h, ',' );
+    for( const auto &c : compsOne ) {
+        auto compsTwo = split( c,  '=' );
+        if( compsTwo.size() == 2 ) {
+            map[ compsTwo[ 0 ]] = removeQuotes( compsTwo[ 1 ]);
+        }
+    }
+    return  std::move( map );
+}
+
 class HttpBinRequester {
 public:
-    HttpBinRequester(std::string userName, std::string password)
+    HttpBinRequester(const std::string &userName, const std::string &password)
         : m_userName{ userName }
         , m_password{ password }
         , m_valid{ false }
@@ -46,15 +121,31 @@ public:
             std::cout << res->body << std::endl;
             printHeaders( *res );
             if( res->status == 401 ) {
-                //calculate the hashes
+                parseAuthInfo( *res );
                 m_valid = true;
             }
         }
     }
 
-    bool post( std::string /*url*/, std::string /*content*/ ) {
+    bool post( std::string url, std::string content ) const {
+        return  false;
+    }
+
+    void parseAuthInfo( httplib::Response &res ) {
+        auto it = res.headers.find( "Www-Authenticate" );
+        if( it != std::end( res.headers )) {
+            auto ah = it->second;
+            if( startsWith( ah, "Digest")) {
+
+            }
+
+//            Digest realm="me@kennethreitz.com", nonce="9a046325ca9ef842370026fc8ab7ad0a", qop="auth", opaque="0c0b7117690cf27a1cb3c3c797dbcfbc", algorithm=MD5, stale=FALSE
+
+        }
 
     }
+
+
 
 
 private:
@@ -100,13 +191,13 @@ std::string readWiki() {
 int main()
 {
     HttpBinRequester req{ "user", "password" };
-    auto wiki = readWiki();
-    if( wiki.size() > 0 ) {
-        auto r = req.post( "/digest-auth/auth/user/password/md5/never", wiki );
-        std::cout << ( r ? "Done" : "Failed" ) << std::endl;
-    } else {
-        std::cout << "Wiki retrieval failed" << std::endl;
-    }
+//    auto wiki = readWiki();
+//    if( wiki.size() > 0 ) {
+//        auto r = req.post( "/digest-auth/auth/user/password/md5/never", wiki );
+//        std::cout << ( r ? "Done" : "Failed" ) << std::endl;
+//    } else {
+//        std::cout << "Wiki retrieval failed" << std::endl;
+//    }
 
 }
 
